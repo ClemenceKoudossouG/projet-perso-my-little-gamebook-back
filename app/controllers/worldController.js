9d3a78419d581dd83b80c21d2dca3dc20d857b87
ClemenceKoudossouG <clemence.koudossou-garde@oclock.school>
2024-02-09 12:48:57 +0100

    update worldController

---
+++ app/controllers/worldController.js
@@ -0,0 +1,37 @@
+// import { worldDataMapper } from "";
+
+const worldController = {
+    // Pour récupérer tous les univers existants.
+    async getAllWorlds(req, res, next) {
+        try {
+            const { result, error } = await worldDataMapper.findAll();
+            // Vérification d'erreur
+            if (error) {
+                next(error);
+            } else {
+            // Renvoi test en JSON si nécessaire
+            res.json(result);
+            }
+        } catch (error) {
+            next(error);
+        }
+    },
+    // Pour récupérer un univers, par son id.
+    async getOneWorld(req, res, next) {
+        try {
+            const { id } = req.params;
+            const { result, error } = await worldDataMapper.findById(id);
+            // Vérification d'erreur
+            if (error) {
+                next(error);
+            } else {
+                // Renvoi test en JSON si nécessaire
+                res.json(result);
+            }
+        } catch (error) {
+            next(error);
+        }
+    }
+};
+
+export default worldController;
