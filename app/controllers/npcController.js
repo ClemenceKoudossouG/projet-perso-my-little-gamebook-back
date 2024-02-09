140ed2a55df78a83f503c5d06e713f47767a0f51
ClemenceKoudossouG <clemence.koudossou-garde@oclock.school>
2024-02-09 15:09:09 +0100

    npcController done

--- app/controllers/npcController.js
+++ app/controllers/npcController.js
@@ -16,6 +16,38 @@ const npcController = {
             next(error);
         }
     },
+    // Pour récupérer un personnage, par son id, en tant qu'utilisateur connecté.
+    async getOneNpc(req, res, next) {
+        try {
+            const { id } = req.params;
+            const { result, error } = await npcDataMapper.findById(id);
+            // Vérification d'erreur
+            if (error) {
+                next(error);
+            } else {
+                // Renvoi test en JSON si nécessaire
+                res.json(result);
+            }
+        } catch (error) {
+            next(error);
+        }
+    },
+    // Pour récupérer tous les personnages existants par univers en tant qu'utilisateur connecté.
+    async getAllNpcsByWorld(req, res, next) {
+        try {
+            const { world } = req.params;
+            const { result, error } = await npcDataMapper.findByLevel(world);
+            // Vérification d'erreur
+            if (error) {
+                next(error);
+            } else {
+                // Renvoi test en JSON si nécessaire
+                res.json(result);
+            }
+        } catch (error) {
+            next(error);
+        }
+    },
 };
 
 export default npcController;
