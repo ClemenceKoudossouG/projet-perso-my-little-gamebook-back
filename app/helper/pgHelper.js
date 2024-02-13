3ff62a1724fefe48ecec750dc7743bfd0259193e
ClemenceKoudossouG <clemence.koudossou-garde@oclock.school>
2024-02-13 15:32:13 +0100

    pgHelper, refacto action dataMapper

---
+++ app/helper/pgHelper.js
@@ -0,0 +1,49 @@
+// On importe pgpool pour pouvoir effectuer les requêtes sql
+import pool from "../services/pgPool.js";
+
+/**
+ * Fonction générique qui retourne les lignes d'une requête SQL
+ * @param {string} sqlQUery - Requêtes SQL
+ * @param {array} values - Valeurs de la requête
+ * @returns
+ */
+
+export async function executeRequest(sqlQuery, values){
+    let result;
+    let error;
+
+    try {
+        // Avec la méthode async/await
+        const response = await pool.query(sqlQuery,values);
+        // On récupère les informations données par la bdd
+        result = response.rows;
+        }
+        catch (err) {
+            error = err;
+        }
+    // On retourne soit le résultat, soit l'erreur
+    return {result,error};
+};
+
+/**
+ * Fonction générique qui retourne la première ligne d'une requête SQL
+ * @param {*} sqlQuery 
+ * @returns 
+ */
+
+export async function executeRequestWithSingleResult(sqlQuery, values){
+    let result;
+    let error;
+
+    try {
+        // Avec la méthode async/await
+        const response = await pool.query(sqlQuery,values);
+        // On récupère les informations données par la bdd
+        result = response.rows[0];
+        }
+        catch (err) {
+            error = err;
+        }
+    // On retourne soit le résultat, soit l'erreur
+    return {result,error};
+};
