90582f1db4118a7b54b728857cb578727daf67cd
ClemenceKoudossouG <clemence.koudossou-garde@oclock.school>
2024-02-14 12:11:20 +0100

    refacto npc dataMapper

--- app/dataMappers/item.js
+++ app/dataMappers/item.js
@@ -1,86 +1,39 @@
 // Le itemDataMapper faisant le lien entre le itemController et les fonctions sql
 
-// On importe pgpool pour pouvoir effectuer les requêtes sql
-import pool from "../services/pgPool.js";
+// Import des fonctions du pgHelper pour refactoriser les fonctions
+import { executeRequest } from "../helper/pgHelper.js";
+import { executeRequestWithSingleResult } from "../helper/pgHelper.js";
 
 // Tout sera contenu dans l'objet itemDataMapper
 const itemDataMapper = {
 
     // Pour récupérer tous les objets existants dans la bdd :
     async findAll() {
-
         // On utilise la fonction sql get_all_items
         const sqlQuery = "SELECT * FROM get_all_items();";
-
-        let result;
-        let error;
-        try {
-            // Avec la méthode async/await
-            const response = await pool.query(sqlQuery);
-
-            // On récupère toutes les rangées en question implémentées dans la bdd
-            result = response.rows;
-        }
-        catch (err) {
-            error = err;
-        }
-
-        // On retourne soit le résultat, soit l'erreur
-        return {result,error};
+        // Appel de la fonction du pgHelper pour exécuter la requête. 
+        return executeRequest(sqlQuery);
     },
 
     // Pour récupérer tous les objets associés à une action existants dans la bdd :
     async findByAction(actionId) {
-
         // On utilise la fonction sql get_all_items_by_action
         const sqlQuery = "SELECT * FROM get_all_items_by_action($1);";
-
         // à laquelle on transfère l'id de l'action donné par le front
         const values = [actionId];
-
-        let result;
-        let error;
-        try {
-            // Avec la méthode async/await
-            const response = await pool.query(sqlQuery,values);
-
-            // On récupère les informations données par la bdd
-            result = response.rows;
-        }
-        catch (err) {
-            error = err;
-        }
-
-        // On retourne soit le résultat, soit l'erreur
-        return {result,error};
+        // Appel de la fonction du pgHelper pour exécuter la requête. 
+        return executeRequest(sqlQuery, values);
     },
 
     // Pour récupérer un objet en particulier :
     async findById(id){
-
         // On utilise la fonction sql get_item_by_id
         const sqlQuery = "SELECT * FROM get_item_by_id($1);";
-
         // à laquelle on transfère l'id de l'objet donné par le front
         const values = [id];
-
-        let result;
-        let error;
-
-        try {
-            // Avec la méthode async/await
-            const response = await pool.query(sqlQuery,values);
-
-            // On récupère les informations données par la bdd
-            result = response.rows[0];
-        } catch(err) {
-            error = err;
-        }
-
-        // On retourne soit le résultat, soit l'erreur
-        return {result,error};
-    },
-
+        // Appel de la fonction du pgHelper pour exécuter la requête. 
+        return executeRequestWithSingleResult(sqlQuery, values);
+    }
 };
 
 // On exporte le itemDataMapper
--- app/dataMappers/npc.js
+++ app/dataMappers/npc.js
@@ -1,86 +1,39 @@
 // Le npcDataMapper faisant le lien entre le npcController et les fonctions sql
 
-// On importe pgpool pour pouvoir effectuer les requêtes sql
-import pool from "../services/pgPool.js";
+// Import des fonctions du pgHelper pour refactoriser les fonctions
+import { executeRequest } from "../helper/pgHelper.js";
+import { executeRequestWithSingleResult } from "../helper/pgHelper.js";
 
 // Tout sera contenu dans l'objet npcDataMapper
 const npcDataMapper = {
 
     // Pour récupérer tous les personnages existants dans la bdd :
     async findAll() {
-
         // On utilise la fonction sql get_all_npcs
         const sqlQuery = "SELECT * FROM get_all_npcs();";
-
-        let result;
-        let error;
-        try {
-            // Avec la méthode async/await
-            const response = await pool.query(sqlQuery);
-
-            // On récupère toutes les rangées en question implémentées dans la bdd
-            result = response.rows;
-        }
-        catch (err) {
-            error = err;
-        }
-
-        // On retourne soit le résultat, soit l'erreur
-        return {result,error};
+        // Appel de la fonction du pgHelper pour exécuter la requête. 
+        return executeRequest(sqlQuery);
     },
 
     // Pour récupérer tous les personnages appartenant à un univers existants dans la bdd :
     async findByWorld(worldId) {
-
         // On utilise la fonction sql get_all_npcs_by_world
         const sqlQuery = "SELECT * FROM get_all_npcs_by_world($1);";
-
         // à laquelle on transfère l'id de l'univers donné par le front
         const values = [worldId];
-
-        let result;
-        let error;
-        try {
-            // Avec la méthode async/await
-            const response = await pool.query(sqlQuery,values);
-
-            // On récupère les informations données par la bdd
-            result = response.rows;
-        }
-        catch (err) {
-            error = err;
-        }
-
-        // On retourne soit le résultat, soit l'erreur
-        return {result,error};
+        // Appel de la fonction du pgHelper pour exécuter la requête. 
+        return executeRequest(sqlQuery, values);
     },
 
     // Pour récupérer un personnage en particulier :
     async findById(id){
-
         // On utilise la fonction sql get_npc_by_id
         const sqlQuery = "SELECT * FROM get_npc_by_id($1);";
-
         // à laquelle on transfère l'id du personnage donné par le front
         const values = [id];
-
-        let result;
-        let error;
-
-        try {
-            // Avec la méthode async/await
-            const response = await pool.query(sqlQuery,values);
-
-            // On récupère les informations données par la bdd
-            result = response.rows[0];
-        } catch(err) {
-            error = err;
-        }
-
-        // On retourne soit le résultat, soit l'erreur
-        return {result,error};
+        // Appel de la fonction du pgHelper pour exécuter la requête. 
+        return executeRequestWithSingleResult(sqlQuery, values);
     },
-
 };
 
 // On exporte le npcDataMapper
