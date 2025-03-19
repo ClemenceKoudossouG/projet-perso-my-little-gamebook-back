d31d0534601a561e9413773c6809f947336b137f
ClemenceKoudossouG <clemence.garde@icloud.com>
2025-03-19 17:07:16 +0100

    config

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports = {
+    development: {
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        host: process.env.PGHOST,
+        port: process.env.PGPORT,
+        dialect: "postgres",
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    },
+};
