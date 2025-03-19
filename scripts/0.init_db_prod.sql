73011e7ca2cb02ef893246e7cb5c663879d7b2f5
ClemenceKoudossouG <clemence.garde@icloud.com>
2025-03-19 13:15:47 +0100

    removed useless comment in init_db script for prod

--- scripts/0.init_db_prod.sql
+++ scripts/0.init_db_prod.sql
@@ -6,7 +6,6 @@ BEGIN
     -- Grant privileges on the database
     EXECUTE format('GRANT ALL PRIVILEGES ON DATABASE %I TO %I', db_name, db_user);
     
-    -- Correct the quoting issue by using `quote_ident`
     EXECUTE format(
         'ALTER DEFAULT PRIVILEGES FOR ROLE %I IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO %I',
         db_user, db_user
